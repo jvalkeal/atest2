0a0b05e1f0d78a479d0040f838df34d5b2b50781
Janne Valkealahti <janne.valkealahti@gmail.com>
2019-12-16 09:48:13 +0000

    test commit

--- spring-cloud-deployer-local/src/test/java/org/springframework/cloud/deployer/spi/local/LocalDeployerProperties2Tests.java
+++ spring-cloud-deployer-local/src/test/java/org/springframework/cloud/deployer/spi/local/LocalDeployerProperties2Tests.java
@@ -24,7 +24,6 @@ public class LocalDeployerProperties2Tests {
 			.withInitializer(context -> {
 				Map<String, Object> map = new HashMap<>();
 				map.put("spring.cloud.deployer.local.working-directories-root", "C:/");
-
 				context.getEnvironment().getPropertySources().addLast(new SystemEnvironmentPropertySource(
 					StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, map));
 			})
